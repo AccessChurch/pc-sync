e69e39b9aa9afa4e618bf4f36123eed001e30466
Kevin Bunnell <kevinbunnell@Kevin-MBP14-5.local>
2026-01-29 15:17:53 -0500

    Fix oauth redirect URI and add calendar scope

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.get('/oauth/start', (req, res) => {
     `?client_id=${PCO_APP_ID}` +
     `&redirect_uri=${encodeURIComponent(PCO_REDIRECT_URI)}` +
     `&response_type=code`;
+    `&scope=calendar`;
 
   res.redirect(authUrl);
 });
